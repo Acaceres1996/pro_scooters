58badd48b1161bdaaf2e35a590e0630c13dd7305
Luis Caceres <agustin.caceres1996@gmail.com>
2019-04-25 15:21:00 -0300

    sync

--- frontend-web-rest/src/main/java/uy/urudin/api/rest/endpoints/ScanEndpoint.java
+++ frontend-web-rest/src/main/java/uy/urudin/api/rest/endpoints/ScanEndpoint.java
@@ -46,8 +46,12 @@ public class ScanEndpoint {
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
 	public Response GetAllRegistro(){
-		List<DTRegistro> ListRegistros = RegistroEJB.findAll();
-		return Response.ok( ListRegistros ).build();
+		List<DTRegistro> ListRegistros = RegistroEJB.findAll();		
+		return Response.status(200).header("Access-Control-Allow-Origin", "*")
+	            .header("Access-Control-Allow-Headers", "origin, content-type, accept, authorization")
+	            .header("Access-Control-Allow-Credentials", "true")
+	            .header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD")
+	            .header("Access-Control-Max-Age", "1209600").entity( ListRegistros ).build();
 	}
 	
 	@GET
