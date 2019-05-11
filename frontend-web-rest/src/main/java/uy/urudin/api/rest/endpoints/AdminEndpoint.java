1b059d3b5ad1c79fda867170d01d0cd9654a222a
Agustin <agustin.caceres1996@gmail.com>
2019-05-11 01:24:23 -0300

    admin login stab

---
+++ frontend-web-rest/src/main/java/uy/urudin/api/rest/endpoints/AdminEndpoint.java
@@ -0,0 +1,23 @@
+package uy.urudin.api.rest.endpoints;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import uy.urudin.datatypes.DTAdmin;
+
+@Path("/admin")
+public class AdminEndpoint {
+	
+	@POST
+	@Path("/login")
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response Login(DTAdmin admin) {	
+		return Response.status(200).entity( admin ).build();
+	}
+		
+}
